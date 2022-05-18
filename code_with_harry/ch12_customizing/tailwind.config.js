fc63f609d030a9e951bb4c68e76a73bef581183d
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-18 11:20:00 +0530

    customizing chapter completed

---
+++ code_with_harry/ch12_customizing/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      xsm: "500px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      spacing: {
+        13: "3.25rem",
+      },
+    },
+  },
+  plugins: [],
+};
